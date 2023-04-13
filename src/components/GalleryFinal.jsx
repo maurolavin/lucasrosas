989914e17b9b90d4fd29446f78b814e1d4e3e1ef
mauro.lavin <villarroelmauroemanuel@gmail.com>
2023-04-12 21:23:39 -0300

    iframe changes. theme correction. frames set to 16 9 aspect ratio

--- src/components/GalleryFinal.jsx
+++ src/components/GalleryFinal.jsx
@@ -58,7 +58,7 @@ function Gallery() {
                     &times;
                 </span>
                 <div className='modalVideoContainer'>
-                <iframe src={selectedProject.embed} alt={selectedProject.description} />
+                <iframe src={selectedProject.embed} alt={selectedProject.description} allow="fullscreen"/>
                 </div>
                 <div className='modalTextContainer'>
                     <h2 className='modal-project'>{selectedProject.project}</h2>
